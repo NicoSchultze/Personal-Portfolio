67070abe4e5a003210cb356fa40d963671d708c5
Nico Schultze <nico.schultze97@gmail.com>
2022-07-17 19:10:32 +0200

    add new github and linkedin connection

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
-import { BsTwitter, BsInstagram } from 'react-icons/bs';
-import { FaFacebookF } from 'react-icons/fa';
+import { BsGithub} from 'react-icons/bs';
+import { SiLinkedin } from "react-icons/si"
+import { BiMessageDetail } from "react-icons/bi"
+
+
+
 
 const SocialMedia = () => (
   <div className="app__social">
     <div>
-      <BsTwitter />
+      <a href="https://www.linkedin.com/in/nico-schultze97/" rel="noreferrer" target="_blank"><SiLinkedin /></a>
     </div>
     <div>
-      <FaFacebookF />
+      <a href="https://github.com/NicoSchultze" rel="noreferrer" target="_blank"><BsGithub /></a>
     </div>
     <div>
-      <BsInstagram />
+      <a href="#contact"><BiMessageDetail /></a>
     </div>
   </div>
 );
