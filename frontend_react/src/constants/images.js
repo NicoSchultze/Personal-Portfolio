acdf87c6efa035fe779ce9f5052087f4373f0c84
Nico Schultze <nico.schultze97@gmail.com>
2022-07-18 19:10:05 +0200

    remove unused images

--- frontend_react/src/constants/images.js
+++ frontend_react/src/constants/images.js
@@ -6,11 +6,6 @@ import python from '../assets/python.png';
 import react from '../assets/react.png';
 import kotlin from "../assets/kotlin.png"
 
-import about01 from '../assets/about01.png';
-import about02 from '../assets/about02.png';
-import about03 from '../assets/about03.png';
-import about04 from '../assets/about04.png';
-
 import profile from '../assets/profile.png';
 import circle from '../assets/circle.svg';
 import logoNico from '../assets/logoNico.png';
@@ -23,10 +18,6 @@ export default {
   python,
   react,
   kotlin,
-  about01,
-  about02,
-  about03,
-  about04,
   profile,
   circle,
   logoNico,
