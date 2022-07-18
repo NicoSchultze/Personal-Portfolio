91aea2f7c70fd2f6f771c583314927e0cb258e89
Nico Schultze <nico.schultze97@gmail.com>
2022-07-18 18:43:35 +0200

    add new profile picture

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -18,7 +18,7 @@ const About = () => {
 
   return (
     <>
-      <h2 className="head-text">I Know that <span>Good Design</span> <br />means  <span>Good Business</span></h2>
+      <h2 className="head-text">These are <span>My Qualities</span> <br />And  <span> My Passions</span></h2>
 
       <div className="app__profiles">
         {abouts.map((about, index) => (
